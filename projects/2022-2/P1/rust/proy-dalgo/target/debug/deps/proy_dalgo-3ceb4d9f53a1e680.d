0e219b2724907a97f6ff40d83ca66b488c0579f3
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-01-22 01:39:55 -0500

    add gitignore.

--- projects/2022-2/P1/rust/proy-dalgo/target/debug/deps/proy_dalgo-3ceb4d9f53a1e680.d
+++
@@ -1,5 +0,0 @@
-/home/xaurdesu/Documentos/repos/Dalgo/projects/2022-2/P1/rust/proy-dalgo/target/debug/deps/proy_dalgo-3ceb4d9f53a1e680: src/main.rs
-
-/home/xaurdesu/Documentos/repos/Dalgo/projects/2022-2/P1/rust/proy-dalgo/target/debug/deps/proy_dalgo-3ceb4d9f53a1e680.d: src/main.rs
-
-src/main.rs:
