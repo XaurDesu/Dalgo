d6aaea570129f086dfb87833a064fa626e15d678
Jaime Torres <andrestbermejoj@gmail.com>
2023-02-06 11:47:49 -0500

    solve 1st problem of t1 in rust

--- projects/2023-1/Homeworks/Taller1/tarea1/src/main.rs
+++ projects/2023-1/Homeworks/Taller1/tarea1/src/main.rs
@@ -1,13 +1,14 @@
 fn matrix_sum (nums: Vec<Vec<i32>>) -> i32 {
     let mut ret = 0;
-    let mut i: i32 = 0;
-    for(i) {
-        
+    for i in 0..nums.len() {
+        ret += nums[i][i];
     }
     return ret;
 }
 
 fn main() {
     println!("Hello, world!");
-    matrix_sum();
+    //maybe not the fanciest way to define this, but i need a sample input you guys, sorry!
+    let nums: Vec<Vec<i32>> = vec![vec![1,0,0],vec![0,1,0],vec![0,0,1]];
+    println!("Matrix sum result is: {}", matrix_sum(nums));
 }   
