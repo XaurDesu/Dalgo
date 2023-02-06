0de8c0dfb48d91a470037415c364681acf8f101b
Jaime Torres <andrestbermejoj@gmail.com>
2023-02-06 11:27:36 -0500

    add t1 structure

---
+++ projects/2023-1/Homeworks/Taller1/tarea1/src/main.rs
@@ -0,0 +1,13 @@
+fn matrix_sum (nums: Vec<Vec<i32>>) -> i32 {
+    let mut ret = 0;
+    let mut i: i32 = 0;
+    for(i) {
+        
+    }
+    return ret;
+}
+
+fn main() {
+    println!("Hello, world!");
+    matrix_sum();
+}   
