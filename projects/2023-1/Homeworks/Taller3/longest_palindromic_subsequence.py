50d3a062ea092d29fc95d9b7669f85d5d198f817
Jaime Torres <andrestbermejoj@gmail.com>
2023-03-12 00:00:52 -0500

    add another solution

--- projects/2023-1/Homeworks/Taller3/longest_palindromic_subsequence.py
+++ projects/2023-1/Homeworks/Taller3/longest_palindromic_subsequence.py
@@ -1,16 +1,13 @@
-def is_palindromic(s):
-    return s == s[::-1]
-def longest_palindromic_subsequence(s):
-    ret = ""
-    strings = []
+from itertools import combinations
 
-    for i in range(0,len(s)):
-        for j in range(i, len(s)+1):
-            sub = s[i:j]
-            if sub in strings:
-                pass
-            strings.append(sub)
-            if len(sub) > len(ret) and is_palindromic(sub):
-                ret = sub
-            
-    return ret
+def get_subsequences(s):
+    out = set()
+    for i in range(1, len(s) + 1):
+        for c in combinations(s, i):
+            if c[::-1] == c:
+                out.add(''.join(c))
+    return sorted(out, reverse=True, key=len)
+
+def longest_palindromic_subsequence(s: str) -> int:
+    strings = get_subsequences(s)    
+    return len(strings[0])
