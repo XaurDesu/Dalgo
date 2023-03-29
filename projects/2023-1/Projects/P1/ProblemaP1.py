aab93bb82c323101c062bf6fe6fe2212fef201f6
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-03-29 15:55:38 -0500

    add some stuffs

--- projects/2023-1/Projects/P1/ProblemaP1.py
+++ projects/2023-1/Projects/P1/ProblemaP1.py
@@ -1,8 +1,17 @@
+import os
+
+ARCHIVOS_PRUEBA = "./tests"
+
+def abrir_archivo(file: str) -> list:
+
+    pass
+
 def beneficiarios(nums: list[int]) -> None:
     pass
 
 def main():
-    pass
+    tests = os.scandir(ARCHIVOS_PRUEBA)
+    print(tests)
 
 if __name__ == "__main__":
     main()
