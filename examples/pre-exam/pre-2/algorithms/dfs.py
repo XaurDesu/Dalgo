45b6602304eaea778a8a48fc9f64ae9fcdfa1c48
Jaime Torres <andrestbermejoj@gmail.com>
2023-04-20 11:21:39 -0500

    add dfs but again

--- examples/pre-exam/pre-2/algorithms/dfs.py
+++ examples/pre-exam/pre-2/algorithms/dfs.py
@@ -1,12 +1,8 @@
-def bfs(Adj, s): 
- visited = [False for v in Adj] 
- queue = [s]
- while 0 < len(queue): 
-   current_node = queue.pop(0)
-   if not visited[current_node]:
-    # Usually here you do something 
-    # with the current node
-    visited[current_node] = True
-    for n in Adj[current_node]:
-      if not visited[n]:
-        queue.append(n)
+def dfs(Adj, s, visited): 
+	
+  # Usually here you do something 	
+  # with the current node 
+  visited[s] = True
+  for n in Adj[s]:
+    if not visited[n]:
+      dfs(Adj,n,visited)
