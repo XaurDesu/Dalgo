7fad1628961a01c0efd4daa0de5ec2f482588e75
Jaime Torres <andrestbermejoj@gmail.com>
2023-04-20 18:15:32 -0500

    add djikstra code

--- examples/pre-exam/pre-2/algorithms/djikstra.py
+++ examples/pre-exam/pre-2/algorithms/djikstra.py
@@ -1,2 +1,28 @@
-def djikstra(adj: list):
-    pass
+import heapq
+
+
+def dijkstra(graph, start, end):
+    """Return the cost of the shortest path between vertices start and end.
+    >>> dijkstra(G, "E", "C")
+    6
+    >>> dijkstra(G2, "E", "F")
+    3
+    >>> dijkstra(G3, "E", "F")
+    3
+    """
+
+    heap = [(0, start)]  # cost from start node,end node
+    visited = set()
+    while heap:
+        (cost, u) = heapq.heappop(heap)
+        if u in visited:
+            continue
+        visited.add(u)
+        if u == end:
+            return cost
+        for v, c in graph[u]:
+            if v in visited:
+                continue
+            next_item = cost + c
+            heapq.heappush(heap, (next_item, v))
+    return -1
