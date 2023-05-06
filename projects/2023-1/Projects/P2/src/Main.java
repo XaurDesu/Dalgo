208597e8fdbb08dbdd30282b86d994b360a09b3b
Jaime Torres <andrestbermejoj@gmail.com>
2023-05-05 19:14:28 -0500

    Finish data collection

--- projects/2023-1/Projects/P2/src/Main.java
+++ projects/2023-1/Projects/P2/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static ArrayList<ArrayList<Integer>> leerArchivo(String path) {
         
-        ArrayList<ArrayList<Integer>> ret = new ArrayList<>(null);
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
         
         try {
             File archivo = new File(path);
@@ -29,7 +29,34 @@ public class Main {
     }
 
     public static void darSolucion(ArrayList<ArrayList<Integer>> casos) {
+        int numCasos = casos.get(0).get(0);
+        System.out.println(numCasos);
 
+        for(int i = 1; i < casos.size(); i++) {
+            
+            if (i >= casos.size()) {
+                break;
+            }
+
+            ArrayList<ArrayList<Integer>> caso = new ArrayList<>();
+            int N = casos.get(i).get(0);
+            int M = casos.get(i).get(1);
+            
+            System.out.println(casos.get(i));
+            i += 1;
+
+            while (casos.get(i).size() == 3) {
+                caso.add(casos.get(i));
+                if (i+1 < casos.size() && casos.get(i+1).size() != 2) {
+                    i += 1;
+                } else {
+                    break;
+                }
+            }
+
+            Solution solve = new Solution(N, M, caso);
+            
+        }
     }
 
     public static void main(String[] args) {
@@ -45,7 +72,8 @@ public class Main {
             if (input.equals("y")) {
                 System.out.println("Por favor, digita la dirección del archivo de prueba.");
                 input = scan.nextLine();
-                leerArchivo(input);
+                ArrayList<ArrayList<Integer>> casos = leerArchivo(input);
+                darSolucion(casos);
                 scan.close();
             } else {
                 scan.close();
--- projects/2023-1/Projects/P2/src/Solution.java
+++ projects/2023-1/Projects/P2/src/Solution.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class Solution {
 
+    int N;
+    int M;
+    ArrayList<ArrayList<Integer>> caso;
     
-    
+    public Solution(int N, int M, ArrayList<ArrayList<Integer>> caso) {
+        this.N = N;
+        this.M = M;
+        this.caso = caso;
+    }
 }
