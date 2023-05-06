93f88008cfa57e5a30e0b8a593ed69450c3492c5
Jaime Torres <andrestbermejoj@gmail.com>
2023-05-05 19:58:33 -0500

    add even more stuff

--- projects/2023-1/Projects/P2/src/Solution.java
+++ projects/2023-1/Projects/P2/src/Solution.java
@@ -6,6 +6,12 @@ public class Solution {
     int M;
     ArrayList<ArrayList<Integer>> caso;
     
+    public void problemSolution() {
+        int N = this.N;
+        int M = this.M;
+        ArrayList<ArrayList<Integer>> caso = this.caso;
+    }
+
     public Solution(int N, int M, ArrayList<ArrayList<Integer>> caso) {
         this.N = N;
         this.M = M;
